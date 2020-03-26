d9e8712425c0a691b37724322f699caf858323df
FranDiego <franciscojosediegoacosta@gmail.com>
2020-03-26 09:42:23 +0100

    data ingest changes John Hopkins Uni has change raw data

--- R/raw_data_path_list.R
+++ R/raw_data_path_list.R
@@ -2,9 +2,9 @@ raw_data_path_list <- function(){
   "https://raw.githubusercontent.com/CSSEGISandData" %>%
     file.path("COVID-19/master/csse_covid_19_data") %>%
     file.path('csse_covid_19_time_series') %>%
-    file.path('time_series_19-covid-') %>%
-    paste0(c('Confirmed','Recovered','Deaths')) %>%
-    paste0('.csv') %>%
-    list(confirmed = .[1], recovered = .[2], deaths = .[3]) %>%
+    file.path('time_series_covid19_') %>%
+    paste0(c('confirmed','deaths','recovered')) %>%
+    paste0('_global.csv') %>%
+    list(confirmed = .[1], deaths = .[2], recovered = .[3]) %>%
     tail(3)
 }
